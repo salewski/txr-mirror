e6d1fb84d5f6c399d72654ee548875316646dd83
Kaz Kylheku <kkylheku@gmail.com>
2009-11-04 12:29:52 -0800

    Got build to work in separate build directory.

--- dep.mk
+++ dep.mk
@@ -1,9 +1,10 @@
-lib.o: lib.h gc.h unwind.h stream.h
-lex.yy.o: y.tab.h lib.h gc.h stream.h parser.h
-regex.o: lib.h unwind.h regex.h
-y.tab.o: lib.h regex.h parser.h
-unwind.o: lib.h gc.h stream.h txr.h unwind.h
-txr.o: lib.h stream.h gc.h unwind.h parser.h match.h txr.h
-match.o: lib.h gc.h unwind.h regex.h stream.h parser.h txr.h match.h
-stream.o: lib.h gc.h unwind.h stream.h
-gc.o: lib.h stream.h txr.h gc.h
+parser.tab.o: $(top_srcdir)/lib.h $(top_srcdir)/regex.h $(top_srcdir)/parser.h
+lib.o: $(top_srcdir)/lib.h $(top_srcdir)/gc.h $(top_srcdir)/unwind.h $(top_srcdir)/stream.h
+lex.yy.o: y.tab.h $(top_srcdir)/lib.h $(top_srcdir)/gc.h $(top_srcdir)/stream.h $(top_srcdir)/parser.h
+regex.o: $(top_srcdir)/lib.h $(top_srcdir)/unwind.h $(top_srcdir)/regex.h
+y.tab.o: $(top_srcdir)/lib.h $(top_srcdir)/regex.h $(top_srcdir)/parser.h
+unwind.o: $(top_srcdir)/lib.h $(top_srcdir)/gc.h $(top_srcdir)/stream.h $(top_srcdir)/txr.h $(top_srcdir)/unwind.h
+txr.o: $(top_srcdir)/lib.h $(top_srcdir)/stream.h $(top_srcdir)/gc.h $(top_srcdir)/unwind.h $(top_srcdir)/parser.h $(top_srcdir)/match.h $(top_srcdir)/txr.h
+match.o: $(top_srcdir)/lib.h $(top_srcdir)/gc.h $(top_srcdir)/unwind.h $(top_srcdir)/regex.h $(top_srcdir)/stream.h $(top_srcdir)/parser.h $(top_srcdir)/txr.h $(top_srcdir)/match.h
+stream.o: $(top_srcdir)/lib.h $(top_srcdir)/gc.h $(top_srcdir)/unwind.h $(top_srcdir)/stream.h
+gc.o: $(top_srcdir)/lib.h $(top_srcdir)/stream.h $(top_srcdir)/txr.h $(top_srcdir)/gc.h
