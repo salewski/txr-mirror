8679f74687ce18576026d48ec4ca5a37c084e0e8
Kaz Kylheku <kaz@kylheku.com>
2010-01-26 20:15:26 -0800

    Version 033.

--- txr.c
+++ txr.c
@@ -43,7 +43,7 @@
 #include "utf8.h"
 #include "txr.h"
 
-const wchar_t *version = L"032";
+const wchar_t *version = L"033";
 const wchar_t *progname = L"txr";
 const wchar_t *spec_file = L"stdin";
 val spec_file_str;
