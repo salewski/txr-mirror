501b00b590d74253346e4c3b17331dcc07a42145
Kaz Kylheku <kaz@kylheku.com>
2010-02-27 11:38:12 +0900

    * lib.h (split_str_sep): Declared.
    * lib.c (split_str_sep): New function.
    (split_str): Semantics changed; the second argument
    is not a set of separator characters (like in split_str_sep)
    but rather a separator string. Fixed bug: if the input
    string is empty, the output list is empty. This caused
    infinite looping behavior in @(freeform).

--- lib.h
+++ lib.h
@@ -308,6 +308,7 @@ val search_str_tree(val haystack, val tree, val start_num, val from_end);
 val sub_str(val str_in, val from_num, val to_num);
 val cat_str(val list, val sep);
 val split_str(val str, val sep);
+val split_str_set(val str, val set);
 val trim_str(val str);
 val string_lt(val astr, val bstr);
 val chr(wchar_t ch);
