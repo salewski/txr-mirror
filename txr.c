d57c2539d8ce40af46050cc16a90165087b9f349
Kaz Kylheku <kaz@kylheku.com>
2011-10-30 15:50:00 -0400

    Version 041

--- txr.c
+++ txr.c
@@ -43,7 +43,7 @@
 #include "utf8.h"
 #include "txr.h"
 
-const wchli_t *version = wli("040");
+const wchli_t *version = wli("041");
 const wchar_t *progname = L"txr";
 const wchar_t *spec_file = L"stdin";
 val spec_file_str;
