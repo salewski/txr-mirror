97b01834e1bf112466360965b0ee5d3e9723c641
Kaz Kylheku <kaz@kylheku.com>
2011-12-23 13:09:54 -0800

    Version 50
    * txr.c (version): Bumped.
    
    * txr.1: Bumped version and set date.
    
    * configure (txr_ver): Bumped.
    
    * RELNOTES: Updated.

--- txr.c
+++ txr.c
@@ -43,7 +43,7 @@
 #include "debug.h"
 #include "txr.h"
 
-const wchli_t *version = wli("049");
+const wchli_t *version = wli("50");
 const wchar_t *progname = L"txr";
 const wchar_t *spec_file = L"stdin";
 val spec_file_str;
