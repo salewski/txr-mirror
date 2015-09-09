0ebd8772c407370e4a135388a0682c32cc85fea0
Kaz Kylheku <kaz@kylheku.com>
2015-09-08 23:20:29 -0700

    linenoise: don't touch OPOST termios output flag.
    * linenoise/linenoise.c (enable_raw_mode): Do not
    clear the OPOST output flag. It is implementation-defined,
    and if it is set, it is probably set for a good reason.

--- linenoise/linenoise.c
+++ linenoise/linenoise.c
@@ -157,8 +157,7 @@ static int enable_raw_mode(lino_t *ls) {
     /* input modes: no break, no CR to NL, no parity check, no strip char,
      * no start/stop output control. */
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    /* output modes - disable post processing */
-    raw.c_oflag &= ~(OPOST);
+    /* we don't change any output modes (c_oflag) */
     /* control modes - set 8 bit chars */
     raw.c_cflag |= (CS8);
     /* local modes - choing off, canonical off, no extended functions,
