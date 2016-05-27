salewski/txr-mirror

commit 68ca87bc780e25dea1418019161d99727225d1ce

* match.c (h_fun, v_fun): Bugfix! copy_list should be used for copying the bindings, not copy_alist. Otherwise functions cannot destructively update a binding, which is useless. We want a function not to manipulate the binding list, but to be able to manipulate the contents of bindings. (match_files_ctx): Declaration moved ahead of match_line. (v_fun): Forward declaration added. (match_line): Allow vertical functions to be called from a horizontal context, in a limited way.
* txr.1: Mention the possibility of a call from a horizontal
context falling back on a vertical function.

match.c
@@ -941,7 +941,7 @@ static val h_fun(match_line_ctx c, match_line_ctx *cout)
     val ub_p_a_pairs = nil;
     val body = cdr(func);
     val piter, aiter;
-    val bindings_cp = copy_alist(c.bindings);
+    val bindings_cp = copy_list(c.bindings);
 
     if (!equal(length(args), length(params)))
       sem_error(elem, lit("function ~a takes ~a argument(s)"),
@@ -1031,6 +1031,15 @@ static val h_eol(match_line_ctx c, match_line_ctx *cout)
   return nil;
 }
 
+typedef struct {
+  val spec, files, bindings, data, data_lineno;
+} match_files_ctx;
+
+static match_files_ctx mf_all(val spec, val files, val bindings,
+                              val data, val data_lineno);
+
+static val v_fun(match_files_ctx *c);
+
 static val match_line(match_line_ctx c)
 {
   for (;;) {
@@ -1095,12 +1104,26 @@ static val match_line(match_line_ctx c)
               c = nc;
               continue;
             } else if (result == decline_k) {
-              if (gethash(v_directive_table, directive))
-                sem_error(elem, lit("~a only exists as a vertical directive"),
-                          directive, nao);
-              else
-                sem_error(elem, lit("no such function or directive: ~a"),
-                          directive, nao);
+              val spec = rlcp(cons(cons(elem, nil), nil), elem);
+              match_files_ctx vc = mf_all(spec, nil, c.bindings, nil, num(0));
+              val vresult = v_fun(&vc);
+
+              if (vresult == next_spec_k) {
+                c.bindings = vc.bindings;
+                break;
+              } else if (vresult == repeat_spec_k) {
+                c.bindings = vc.bindings;
+                continue;
+              } else if (vresult == decline_k) {
+                if (gethash(v_directive_table, directive))
+                  sem_error(elem, lit("~a only exists as a vertical directive"),
+                            directive, nao);
+                else
+                  sem_error(elem, lit("no such function or directive: ~a"),
+                            directive, nao);
+              } else {
+                return vresult;
+              }
             } else {
               return result;
             }
@@ -1538,10 +1561,6 @@ static void do_output(val bindings, val specs, val filter, val out)
     put_char(out, chr('\n'));
   }
 }
-
-typedef struct {
-  val spec, files, bindings, data, data_lineno;
-} match_files_ctx;
 
 static match_files_ctx mf_all(val spec, val files, val bindings,
                               val data, val data_lineno)
@@ -2889,7 +2908,7 @@ static val v_fun(match_files_ctx *c)
     val ub_p_a_pairs = nil;
     val body = cdr(func);
     val piter, aiter;
-    val bindings_cp = copy_alist(c->bindings);
+    val bindings_cp = copy_list(c->bindings);
 
     debug_check(specline, c->bindings, if2(consp(c->data), car(c->data)), c->data_lineno, nil);
 

txr.1
@@ -3021,8 +3021,13 @@ Example:
 
   Output:         fun="horizontal"
 
-A call made in a clearly horizontal context will ignore
-the vertical definition, if any, and require the horizontal function.
+A call made in a clearly horizontal context will prefer the
+horizontal function, and only fall back on the vertical one
+if the horizontal one doesn't exist. (In this fall-back case,
+the vertical function is called with empty data; it is useful
+for calling vertical functions which process arguments and
+produce values.)
+
 In the next example, the call is followed by trailing material,
 placing it in a horizontal context. Leading material will
 do the same thing:

commit 3d9e60cb8ba2a9698336ae4697f59e14282a673d

Fix 2011-11-17 regression.
This addresses a regression caused by commit
68ca87bc780e25dea1418019161d99727225d1ce, affecting a hundred
releases of TXR from 42 to 141.

The copy_alist call to deeply clone the variable bindings was
carelessly replaced with copy_list, to address the problem
that functions weren't able to mutate outer bindings.

However, the parameter binding code depends on the deep
cloning; it destructively manipulates bindings with the
understanding that they are copies that will be thrown away.

Test case:

@(define f0 (var))
@(end)
@(define f1 (out1 out2 var))
@  (bind out1 `a:@var`)
@  (maybe)
@    (f0 `b:@var`)
@  (end)
@  (bind out2 `a:@var`)
@(end)
@(f1 o1 o2 "z")

This program incorrectly binds o1 and o2 to different
strings, "a:z" and "a:b:z" respectively, because
the f0 pattern function call has the unwanted effect of
mutating var. Expected behavior is that both o1 and
o2 are bound to identical strings, "a:z" and "a:z".

* match.c (h_fun, v_fun): When binding arguments, extend
the environment with new bindings instead of trying to
re-use existing ones, because we do not have a copy of the
binding cells, like we used to when bindings_cp was
produced by a copy_alist call.

match.c
@@ -1139,14 +1139,14 @@ static val h_fun(match_line_ctx *c)
       if (arg && bindable(arg)) {
         val val = tx_lookup_var(arg, c->bindings);
         if (val) {
-          bindings_cp = acons_new(param, cdr(val), bindings_cp);
+          bindings_cp = cons(cons(param, cdr(val)), bindings_cp);
         } else {
           bindings_cp = alist_nremove1(bindings_cp, param);
           ub_p_a_pairs = cons(cons(param, arg), ub_p_a_pairs);
         }
       } else {
         val val = txeval(elem, arg, c->bindings);
-        bindings_cp = acons_new(param, val, bindings_cp);
+        bindings_cp = cons(cons(param, val), bindings_cp);
       }
     }
 
@@ -3608,14 +3608,14 @@ static val v_fun(match_files_ctx *c)
       if (bindable(arg)) {
         val val = tx_lookup_var(arg, c->bindings);
         if (val) {
-          bindings_cp = acons_new(param, cdr(val), bindings_cp);
+          bindings_cp = cons(cons(param, cdr(val)), bindings_cp);
         } else {
           bindings_cp = alist_nremove1(bindings_cp, param);
           ub_p_a_pairs = cons(cons(param, arg), ub_p_a_pairs);
         }
       } else {
         val val = txeval(specline, arg, c->bindings);
-        bindings_cp = acons_new(param, val, bindings_cp);
+        bindings_cp = cons(cons(param, val), bindings_cp);
       }
     }
 