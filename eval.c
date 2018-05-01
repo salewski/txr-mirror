salewski/txr-mirror

commit ca6199a0a48cba367ebc81e6d463c4e0a170f9c5

eval: bugs/regressions in handling nil in param expansion.
Due to a number of changes since December 2016, we have two
problems in the evaluator: (lambda (nil)) is accepted rather
than complaining that nil is not bindable; and
(tree-bind (()) '(3)) silently proceeds rather than
diagnosing that the atom 3 doesn't match the empty pattern ().

* eval.c (expand_opt_params_rec, expand_params_rec): When not
expandin macro-style params, check for a parameter not being
bindable, and also avoid recursion entirely when not doing
macro style.
(bind_macro_params): Don't assume that an atom parameter is
a variable, but rather tha a non-list parameter is
a variable. Otherwise we bind nil rather than treating it
as an empty sub-pattern. Before the December 2016 change
(744340ab), we were checking bindablep here; the idea was to
hoist the detailed checking to expansion time. But then the
pattern versus variable distinction was bungled here.

eval.c
@@ -914,14 +914,14 @@ static val expand_opt_params_rec(val params, val menv,
           return params;
         return rlcp(cons(pair, params_ex), cdr(params));
       }
-    } else if (!macro_style_p && consp(car(pair))) {
+    } else if (!macro_style_p && !bindable(car(pair))) {
       eval_error(form, lit("~s: parameter symbol expected, not ~s"),
                  car(form), car(pair), nao);
     } else {
       val param = car(pair);
-      val param_ex = expand_params_rec(param, menv,
-                                       macro_style_p,
-                                       form, pspecials);
+      val param_ex = if3(macro_style_p && consp(param),
+                         expand_params_rec(param, menv, t, form, pspecials),
+                         param);
       val initform = cadr(pair);
       val initform_ex = rlcp(expand(initform, menv), initform);
       val opt_sym = caddr(pair);
@@ -987,9 +987,13 @@ static val expand_params_rec(val params, val menv,
         eval_error(form, lit("~s: ~s parameter requires bindable symbol"),
                    car(form), param, nao);
       param_ex = param;
-    } else {
-      param_ex = expand_params_rec(param, menv, macro_style_p, form, pspecials);
+    } else if (macro_style_p && (nilp(param) || consp(param))) {
+      param_ex = expand_params_rec(param, menv, t, form, pspecials);
       new_menv = make_var_shadowing_env(menv, get_param_syms(param_ex));
+    } else if (!bindable(param)) {
+      not_bindable_error(form, param);
+    } else {
+      param_ex = param;
     }
 
     {
@@ -1232,7 +1236,7 @@ static val bind_macro_params(val env, val menv, val params, val form,
         goto noarg;
       }
 
-      if (atom(param)) {
+      if (!listp(param)) {
         env_vbind(new_env, param, car(form));
       } else {
         if (optargs) {
@@ -1276,7 +1280,7 @@ static val bind_macro_params(val env, val menv, val params, val form,
     }
 
 noarg:
-    if (atom(param)) {
+    if (!listp(param)) {
       env_vbind(new_env, param, nil);
     } else {
       val nparam = pop(&param);

commit c56d0c84dfe150f5dfe513be2dd9ce105684404b

expander: regression in param list treatment.
Change ca6199a that went into TXR 191 was over-zealous in suppressing
expand_param_rec calls for non-macro argument processing. The recursive
calls are needed in order to detect parameters that are special
variables and add them to pspecials. The upshot is that usage such as
the following broke: (lambda (*stdout*) ...) because expand_param_rec
isn't called on the *stdout* symbol and *stdout* is thus not noted as a
special variable, and thus the expander then neglects to produce
(sys:with-dyn-rebinds (*stdout*) ...) around the body of the lambda.

The new compiler introduced at the same time made this harder
to find. Why? Because the compiler ignores the
sys:with-dyn-rebinds special form; it implements its own
handling for specials in lambda and let! So compiled code is
unaffected by this regression.

* eval.c (expand_opt_params_rec): Call expand_params_rec on the
car of the optional var-init pair, whether or not expanding a macro
style parameter.
(expand_params_rec): Call expand_params_rec recursively on any parameter
that is bindable whether or not in macro mode. The two cases fold
together again, and so here we see that the recent fix d934a3e was also
a regression caused by ca6199a.

eval.c
@@ -920,9 +920,9 @@ static val expand_opt_params_rec(val params, val menv,
                  car(form), car(pair), nao);
     } else {
       val param = car(pair);
-      val param_ex = if3(macro_style_p && consp(param),
-                         expand_params_rec(param, menv, t, form, pspecials),
-                         param);
+      val param_ex = expand_params_rec(param, menv,
+                                       macro_style_p,
+                                       form, pspecials);
       val initform = cadr(pair);
       val initform_ex = rlcp(expand(initform, menv), initform);
       val opt_sym = caddr(pair);
@@ -988,14 +988,11 @@ static val expand_params_rec(val params, val menv,
         eval_error(form, lit("~s: ~s parameter requires bindable symbol"),
                    car(form), param, nao);
       param_ex = param;
-    } else if (macro_style_p && (nilp(param) || consp(param))) {
+    } else if (bindable(param) || (macro_style_p && listp(param))) {
       param_ex = expand_params_rec(param, menv, t, form, pspecials);
       new_menv = make_var_shadowing_env(menv, get_param_syms(param_ex));
-    } else if (!bindable(param)) {
-      not_bindable_error(form, param);
     } else {
-      param_ex = param;
-      new_menv = make_var_shadowing_env(menv, cons(param, nil));
+      not_bindable_error(form, param);
     }
 
     {