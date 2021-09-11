salewski/txr-mirror

commit 3d5d525eb525cfad8f643917c31e3d9fedce2874

sockets: improve socked type patch.
* socket.c (SOCK_NONBLOCK, SOCK_CLOEXEC): #define these as 0 if
they are missing.
(open_socket, socketpair_wrap): Clear the SOCK_NONBLOCK and
SOCK_CLOEXEC flags in a single operation instead of two. Use C
native arithmetic instead of Lisp logand and lognot.

socket.c
@@ -1090,15 +1090,20 @@ static val sock_recv_timeout(val sock, val usec)
 }
 #endif
 
+#ifndef SOCK_NONBLOCK
+#define SOCK_NONBLOCK 0
+#endif
+
+#ifndef SOCK_CLOEXEC
+#define SOCK_CLOEXEC 0
+#endif
+
 static val open_socket(val family, val type, val mode_str)
 {
   val self = lit("open-socket");
   int fd = socket(c_num(family, self), c_num(type, self), 0);
-#ifdef SOCK_NONBLOCK
-  type = logand(type, lognot(num_fast(SOCK_NONBLOCK), nil));
-#endif
-#ifdef SOCK_CLOEXEC
-  type = logand(type, lognot(num_fast(SOCK_CLOEXEC), nil));
+#if SOCK_NONBLOCK || SOCK_CLOEXEC
+  type = num_fast(c_num(type, self) & ~(SOCK_NONBLOCK | SOCK_CLOEXEC));
 #endif
   return open_sockfd(num(fd), family, type, mode_str, self);
 }
@@ -1116,11 +1121,8 @@ static val socketpair_wrap(val family, val type, val mode_str)
     uw_throwf(socket_error_s, lit("~a failed: ~d/~s"),
               self, num(errno), errno_to_str(errno), nao);
 
-#ifdef SOCK_NONBLOCK
-  type = logand(type, lognot(num_fast(SOCK_NONBLOCK), nil));
-#endif
-#ifdef SOCK_CLOEXEC
-  type = logand(type, lognot(num_fast(SOCK_CLOEXEC), nil));
+#if SOCK_NONBLOCK || SOCK_CLOEXEC
+  type = num_fast(c_num(type, self) & ~(SOCK_NONBLOCK | SOCK_CLOEXEC));
 #endif
 
   {

commit c9006f7963382ba4ff4a0b37271f1cfbaae583d7

sockets: bug in clearing SOCK_* type flags.
Paul A. Patience reports a regression in
3d5d525eb525cfad8f643917c31e3d9fedce2874, whereby the code
marked with #if SOCK_NONBLOCK || SOCK_CLOEXEC is being
excluded by the preprocoessor, and so those flags are not
being cleared from the socket type that we retain.

This is because these preprocessor symbols are not necessarily
integer constants. They may expand to C enum identifiers,
in which case, in preprocessor expressions they appear to take
on the value zero.

* socket.c (open_socket, socketpair_wrap): Use the if
statement to test for SOCK_NONBLOCK or SOCK_CLOEXEC being
nonzero, rather than a preprocessor #if. This should still be
optimized away as unreachable code if they are zero.

* tests/014/socket-misc.tl: New file.

socket.c
@@ -1107,9 +1107,9 @@ static val open_socket(val family, val type, val mode_str)
     uw_ethrowf(socket_error_s, lit("~a failed: ~d/~s"),
                self, num(errno), errno_to_str(errno), nao);
 
-#if SOCK_NONBLOCK || SOCK_CLOEXEC
-  type = num_fast(c_num(type, self) & ~(SOCK_NONBLOCK | SOCK_CLOEXEC));
-#endif
+  if (SOCK_NONBLOCK || SOCK_CLOEXEC)
+    type = num_fast(c_num(type, self) & ~(SOCK_NONBLOCK | SOCK_CLOEXEC));
+
   return open_sockfd(num(fd), family, type, mode_str, self);
 }
 
@@ -1126,9 +1126,8 @@ static val socketpair_wrap(val family, val type, val mode_str)
     uw_ethrowf(socket_error_s, lit("~a failed: ~d/~s"),
                self, num(errno), errno_to_str(errno), nao);
 
-#if SOCK_NONBLOCK || SOCK_CLOEXEC
-  type = num_fast(c_num(type, self) & ~(SOCK_NONBLOCK | SOCK_CLOEXEC));
-#endif
+  if (SOCK_NONBLOCK || SOCK_CLOEXEC)
+    type = num_fast(c_num(type, self) & ~(SOCK_NONBLOCK | SOCK_CLOEXEC));
 
   {
     val s0 = open_sockfd(num(sv[0]), family, type, mode_str, self);

tests/014/socket-misc.tl
@@ -1,0 +1,4 @@
+(load "../common")
+
+(with-stream (s (open-socket af-inet (logior sock-dgram sock-nonblock)))
+  (test (sock-listen s) t))