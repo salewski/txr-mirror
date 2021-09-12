7c174401f4926683d26e5b638b95d8956cfdb211
Paul A. Patience <paul@apatience.com>
2021-09-12 00:43:22 -0400

    defset: add set-mask and clear-mask.
    * stdlib/defset.tl (set-mask, clear-mask): New update macros.
    
    * stdlib/optimize.tl (calc-liveness): Use the new macros.
    
    * stdlib/socket.tl (sys:str-inaddr-net-impl, str-in6addr-net):
    Same.
    
    * stdlib/termios.tl (set-iflags, set-oflags, set-cflags,
    set-lflags, clear-iflags, clear-oflags, clear-cflags,
    clear-lflags): Same.
    
    * lisplib.c (defset_set_entries): Add set-mask and clear-mask
    to autoload symbols for defset.
    
    * txr.1: Documented.
    
    * stdlib/doc-syms.tl: Updated.

--- lisplib.c
+++ lisplib.c
@@ -809,6 +809,7 @@ static val defset_set_entries(val dlt, val fun)
   val name[] = {
     lit("defset"), lit("sub-list"), lit("sub-vec"), lit("sub-str"),
     lit("left"), lit("right"), lit("key"),
+    lit("set-mask"), lit("clear-mask"),
     nil
   };
   set_dlt_entries(dlt, name, fun);
