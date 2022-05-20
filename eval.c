c6619399afc67006f7fa63d895d38a6c7008a6af
Kaz Kylheku <kaz@kylheku.com>
2022-05-20 07:19:37 -0700

    eval: remove message about --backtrace option.
    * eval.c (error_trace): Don't mention that --backtrace can be
    used to enable backtraces. This is not only a nuisance, but
    appears in packaged programs which do not support such an option,
    making it highly misleading.

--- eval.c
+++ eval.c
@@ -512,11 +512,7 @@ void error_trace(val exsym, val exvals, val out_stream, val prefix)
   if (dbg_backtrace) {
     format(out_stream, lit("~a backtrace:\n"), prefix, nao);
     debug_dump_backtrace(out_stream, prefix);
-  } else {
-    format(std_error, lit("~a run with --backtrace to enable backtraces\n"), prefix, nao);
   }
-#else
-  format(std_error, lit("~a not compiled with backtrace support\n"), prefix, nao);
 #endif
 
   if (max_length) {
